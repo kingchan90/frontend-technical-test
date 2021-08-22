b4491fb5466a043cfc6e3ae0d6530b66db01f366
quan.tran <quan.tran@it-consultis.com>
2021-08-23 00:16:46 +0700

    [Question 3]: Updated the job list data.

--- src/components/JobList/JobItem.tsx
+++ src/components/JobList/JobItem.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-
+import { printDate } from "../../utils/helpers";
 interface JobItemProps {
-  name: String,
-  id: Number,
-  location: String,
-  locations_count: Number
+  name: string,
+  id: number,
+  location: string,
+  locations_count: number,
+  start: string,
+  end: string
 }
-const JobItem:React.FC<JobItemProps> = (props: JobItemProps) => {
-  const { id, name, location, locations_count } = props;
+const JobItem: React.FC<JobItemProps> = (props: JobItemProps) => {
+  const { id, name, location, locations_count, start, end } = props;
+  const dateObj = printDate(start, end);
+  const { date, time } = dateObj;
   return (
     <div className="job-item">
       <div className="title text-bold">{name} <span className="id">{`(Job #${id})`}</span></div>
       <div className="location">{location}</div>
-      <div className="date">Sat Sep 01 2018</div>
-      <div className="text-bold">20:00 - 20:15</div>
-      {locations_count && <div className="rounding text-blue location-count">
+      <div className="date">{date}</div>
+      {time ? <div className="text-bold">{time}</div> : <></>}
+      {locations_count ? <div className="rounding text-blue location-count">
         {locations_count}
-      </div>}
+      </div> : <></>}
     </div>
   )
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,16 @@
-import {
-  Job,
-  JobAllocations
-} from "../common/types";
+import { Job, JobAllocations } from "../common/types";
+import dayjs from "dayjs";
 
-export const mapAllocationToJob = (jobs: Job[], jobAllocations: JobAllocations[]) => {
+/**
+ * Transger jobAllocations to the Job array.
+ * @param {Job} jobs jobs array.
+ * @param {JobAllocations} jobAllocations jobAllocations array.
+ * @return {Array} The transfered jobs array
+ */
+export const mapAllocationToJob = (
+  jobs: Job[],
+  jobAllocations: JobAllocations[]
+) => {
   return jobs.map((job) => {
     const { id: JobId } = job;
     return {
@@ -15,4 +22,28 @@ export const mapAllocationToJob = (jobs: Job[], jobAllocations: JobAllocations[]
       ).length,
     };
   });
+};
+
+/**
+ * Print out the date
+ * @param {string} start start date string.
+ * @param {string} end end date string.
+ * @return {objcet} The sum of the two parameters.
+ */
+
+export const printDate = (start: string, end: string) => {
+  // check if start is same date to end
+  const startObj = dayjs(start);
+  const endObj = dayjs(end);
+  const isSameDate: boolean = !startObj.diff(end, "day");
+  if (isSameDate) {
+    return {
+      date: startObj.format("ddd MMM D YYYY"),
+      time: `${startObj.hour()} - ${endObj.hour()}`,
+    };
+  } 
+  return {
+    date: `${startObj.format("ddd MMM D YYYY")} - ${endObj.format("ddd MMM D YYYY")}`,
+    time: ''
+  }
 };
